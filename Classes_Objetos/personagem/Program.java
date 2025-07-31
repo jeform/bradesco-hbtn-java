13152d4678b78a9ae4fa42384e98afae2c64739d
jesse machado <jesse.machado@bradesco.com.br>
2025-07-31 14:50:50 -0300

    Remove PersonagemGame, Program classes, and README.md files

--- Classes_Objetos/personagem/Program.java
+++ Classes_Objetos/personagem/Program.java
@@ -1,4 +1,5 @@
-package Classes_Objetos;
+package Classes_Objetos.personagem;
+
 public class Program {
     public static void main(String[] args) {
         PersonagemGame personagem1 = new PersonagemGame();
