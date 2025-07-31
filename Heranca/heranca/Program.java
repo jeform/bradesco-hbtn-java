2d2119e3afb5d2f51f15999284661dd6323c6312
jesse machado <jesse.machado@bradesco.com.br>
2025-07-31 20:50:26 -0300

    Fix typo in output message and add compiled class files for Animal and Cachorro

--- Heranca/heranca/Program.java
+++ Heranca/heranca/Program.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 
     System.err.println("meuAnimal eh um " + meuAnimal.getClass().getName());
     System.err.println("meuCachorro eh um " + meuCachorro.getClass().getName());
-    System.err.println("objeto eh um " + obj.getClass().getName());
+    System.err.println("obj eh um " + obj.getClass().getName());
     System.err.println("umOutroAnimal eh um " + umOutroAnimal.getClass().getName());
     }
 }
