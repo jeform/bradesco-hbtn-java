404d4efdc43bd50c7a88f0b2f2811b0b327d9e66
jesse machado <jesse.machado@bradesco.com.br>
2025-07-31 20:22:23 -0300

    Add Program.java with main method and class instantiation

---
+++ Heranca/heranca/Program.java
@@ -0,0 +1,13 @@
+public class Program{
+public static void main(String[] args) {
+    Animal meuAnimal = new Animal();
+    Cachorro meuCachorro = new Cachorro();
+    Object obj = meuCachorro;
+    Animal umOutroAnimal = meuCachorro; 
+
+    System.err.println("meuAnimal eh um " + meuAnimal.getClass().getName());
+    System.err.println("meuCachorro eh um " + meuCachorro.getClass().getName());
+    System.err.println("objeto eh um " + obj.getClass().getName());
+    System.err.println("umOutroAnimal eh um " + umOutroAnimal.getClass().getName());
+    }
+}
