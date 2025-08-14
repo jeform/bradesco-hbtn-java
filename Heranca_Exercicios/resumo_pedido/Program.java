b043da1835d17ab92142057b0d43c878e2d600e5
jesse machado <jesse.machado@bradesco.com.br>
2025-08-14 16:55:06 -0300

    Ajuste formatação

--- Heranca_Exercicios/resumo_pedido/Program.java
+++ Heranca_Exercicios/resumo_pedido/Program.java
@@ -6,11 +6,11 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
         produtos.Livro livro1 = new produtos.Livro("Duna", 2017, "Brasil", 56.30, 680, "Frank Herbert", 1);
-        produtos.Livro livro2 = new produtos.Livro("Um de nós está mentindo", 2018, "Brasil", 34.93, 384, "Karen Mcmanus", 1);
-        produtos.Livro livro3 = new produtos.Livro("Mindset Milionário", 2021, "Brasil", 31.70, 272, "José Roberto Marques", 1);
+        produtos.Livro livro2 = new produtos.Livro("Um de nos esta mentindo", 2018, "Brasil", 34.93, 384, "Karen Mcmanus", 1);
+        produtos.Livro livro3 = new produtos.Livro("Mindset Milionario", 2021, "Brasil", 31.70, 272, "Jose Roberto Marques", 1);
 
-        produtos.Dvd dvd1 = new produtos.Dvd("Anjos da Noite 5 - Guerras de Sangue", 2016, "Estados Unidos", 16.90, "Anna Foerster", "Ação", 91);
-        produtos.Dvd dvd2 = new produtos.Dvd("Annabelle 2 - A Criação do Mal", 2017, "Brasil", 39.90, "James Wan e Peter Safran", "Terror", 109);
+        produtos.Dvd dvd1 = new produtos.Dvd("Anjos da Noite 5 - Guerras de Sangue", 2016, "Estados Unidos", 16.90, "Anna Foerster", "Acao", 91);
+        produtos.Dvd dvd2 = new produtos.Dvd("Annabelle 2 - A Criacao do Mal", 2017, "Brasil", 39.90, "James Wan e Peter Safran", "Terror", 109);
 
         ItemPedido[] itensPedido1 = new ItemPedido[]{
             new ItemPedido(livro1, 1),
@@ -33,7 +33,7 @@ public class Program {
         };
         Pedido pedido3 = new Pedido(5, itensPedido3);
         pedido3.apresentarResumoPedido();
-        
+
         ItemPedido[] itensPedido4 = new ItemPedido[]{
             new ItemPedido(livro2, 1),
             new ItemPedido(livro2, 1),
