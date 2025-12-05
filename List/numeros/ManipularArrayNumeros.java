99cc13cf415fd286f8b730704239b16a30d01481
jesse machado <jesse.machado@bradesco.com.br>
2025-12-05 20:13:29 -0300

    adiciona classe ManipularArrayNumeros com métodos para manipulação de listas de números e atualiza classe Program para tratamento de exceções

---
+++ List/numeros/ManipularArrayNumeros.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class ManipularArrayNumeros {
+    
+    public static int buscarPosicaoNumero(List<Integer> numeros, int numero) {
+        for (int i = 0; i < numeros.size(); i++) {
+            if (numeros.get(i) == numero) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public static void adicionarNumero(List<Integer> numeros, int numero) {
+        if (buscarPosicaoNumero(numeros, numero) != -1) {
+            throw new IllegalArgumentException("Numero jah contido na lista");
+        }
+        numeros.add(numero);
+    }
+    
+    public static void removerNumero(List<Integer> numeros, int numero) {
+        int posicao = buscarPosicaoNumero(numeros, numero);
+        if (posicao == -1) {
+            throw new IllegalArgumentException("Numero nao encontrado na lista");
+        }
+        numeros.remove(posicao);
+    }
+    
+    public static void substituirNumero(List<Integer> numeros, int numeroSubstituir, int numeroSubstituto) {
+        int posicao = buscarPosicaoNumero(numeros, numeroSubstituir);
+        if (posicao != -1) {
+            numeros.set(posicao, numeroSubstituto);
+        } else {
+            numeros.add(numeroSubstituto);
+        }
+    }
+}
