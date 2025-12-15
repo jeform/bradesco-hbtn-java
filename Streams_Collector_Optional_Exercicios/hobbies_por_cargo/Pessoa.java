638428c979affd6407821d9dca6a253e8006429d
jesse machado <jesse.machado@bradesco.com.br>
2025-12-14 22:52:14 -0300

    adiciona classes Pessoa e ConsultaPessoas com métodos para gerenciamento de pessoas, incluindo contagem, média salarial e hobbies por cargo

---
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/Pessoa.java
@@ -0,0 +1,56 @@
+import java.util.Locale;
+import java.util.List;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+    private List<String> hobbies;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario, List<String> hobbies) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+        this.hobbies = hobbies;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    @Override
+    public int compareTo(Pessoa outra) {
+        return this.nome.compareTo(outra.nome);
+    }
+
+    @Override
+    public String toString() {
+        Locale localeBR = new Locale("pt", "BR");
+        return String.format(localeBR, "[%d] %s %s %d R$ %f", 
+            codigo, nome, cargo, idade, salario);
+    }
+}
